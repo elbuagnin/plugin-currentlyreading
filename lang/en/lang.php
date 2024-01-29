6933cc044363a4e3e5a62e0578234bdc0904d634
Billy <billybaier@free.fr>
2024-01-29 23:20:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,12 +1,15 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * English language file for currentlyreading plugin
  *
+ * @author Billy <billybaier@free.fr>
  * @author Charles Fewlass <fewlass@suddenlink.net>
  */
+$lang['currentlyreading']      = 'https://blackhawkfloors.com/when-should-you-opt-for-hardwood-flooring-refinishing/
 
-// menu entry for admin plugins
-// $lang['menu'] = 'Your menu entry';
-
-// custom language strings for the plugin
-$lang['currentlyreading'] = 'Currently Reading';
+You coukd definitely see your enthusiasm in the work you 
+write. The arena hopes for more passionate writers such as you who aren\'t 
+afraid to mention how they believe. At all times follow your heart.';
